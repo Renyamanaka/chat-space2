072881ba91252a8ab7257e1ed5337abfa8a94035
Renyamanaka <ren.yamanaka0726@gmail.com>
2020-07-09 16:57:08 +0900

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,14 +28,3 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 end
-
-
-
-input = gets.to_i
-
-if input <== 0
-  puts "0以下の数字です"
-elsif input <== 10
-  puts "10以下の数字です"
-else
-  puts "10より大きい数字です"
